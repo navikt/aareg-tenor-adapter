44b3a9d63002ced5bbc18660b21c4f6a80ed4520
Knut Arild Slåtsve <knut.arild.slatsve@nav.no>
2026-01-20 11:43:48 +0100

    wip

--- src/main/java/no/nav/aareg/tenor/consumer/aareg/AaregServicesConsumer.java
+++ src/main/java/no/nav/aareg/tenor/consumer/aareg/AaregServicesConsumer.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.aareg.tenor.consumer.aareg.domain.v2.Arbeidsforhold;
 import no.nav.aareg.tenor.consumer.texas.TexasConsumer;
+import no.nav.aareg.tenor.exception.ApplicationException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpClientErrorException;
@@ -56,9 +57,11 @@ public class AaregServicesConsumer {
                         httpHeaders.setBearerAuth(texasConsumer.hentToken(TexasConsumer.IDP_AZURE_AD, aaregServicesScope));
                     })
                     .retrieve()
+                    .onStatus(status -> status.value() == 404, (_, _) -> {
+                       throw new ApplicationException("Fant ikke arbeidsforhold med id " + navArbeidsforholdId);
+                    })
                     .body(Arbeidsforhold.class);
-        } catch (HttpClientErrorException.NotFound e) {
-            log.info("aareg-services returnerte 404 for arbeidsforhold med id {}", navArbeidsforholdId);
+        } catch (ApplicationException e) {
             return null;
         }
     }
